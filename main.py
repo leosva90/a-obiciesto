c59455b92016226205c2b5b8cd3fd42be828c29c
unknown <leosva90@gmail.com>
2022-07-28 12:55:40 -0300

    first commit

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
-from pickle import FALSE
+def imc(estatura, peso):
+    return peso/ estatura**2
+
+peso = float(input('Escriba su peso (kg):'))
+estatura = float(input('Escriba su estatura (M):'))
+
+indice = imc(estatura,peso)
+
+print('Su Indice de masa corporal es: {}'.format(indice))
 
 
-Nombre = "Leonel"
 
-Apellidos = "Saavedra"
-
-Edad = 32
-
-Email = "leosva90@gmail.com"
-
-Teléfono = 3518035575
-
-Casado = False
-
-Hijos = False
-
-amigos = ["Facu","Jony","Emi","Mati"]
-
-Películas = {"primera":"Gladiador","segunda":"Hombres de honor","tercera":"Señor de los anillos"}
-
-print("Mi nombre y apellido es:", Nombre , Apellidos, "y tengo:",Edad,"años", "mi número de celular es:",Teléfono,"¿Estoy casado?",(bool(Casado)), 
-      "¿Tengo hijos?",(bool(Hijos)),". " "Mis amigos son",amigos, "y mis peliculas favoritas son en este orden:", Películas)
 
 
