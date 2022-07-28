8584f4caf09ff5eeaad80bf011c09dd86094107e
unknown <leosva90@gmail.com>
2022-07-28 12:43:59 -0300

    fisrt commit

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-x = "Imprimiendo variable"
-x = "modificando variable"
-print(str(x))
+from pickle import FALSE
+
+
+Nombre = "Leonel"
+
+Apellidos = "Saavedra"
+
+Edad = 32
+
+Email = "leosva90@gmail.com"
+
+Teléfono = 3518035575
+
+Casado = False
+
+Hijos = False
+
+amigos = ["Facu","Jony","Emi","Mati"]
+
+Películas = {"primera":"Gladiador","segunda":"Hombres de honor","tercera":"Señor de los anillos"}
+
+print("Mi nombre y apellido es:", Nombre , Apellidos, "y tengo:",Edad,"años", "mi número de celular es:",Teléfono,"¿Estoy casado?",(bool(Casado)), 
+      "¿Tengo hijos?",(bool(Hijos)),". " "Mis amigos son",amigos, "y mis peliculas favoritas son en este orden:", Películas)
 
 
