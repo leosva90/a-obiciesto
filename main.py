c826a97272509fb588112fcea539286cbb22c8f0
unknown <leosva90@gmail.com>
2022-07-30 20:04:46 -0300

    first commit

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
-for i in range(1,30):
-	if i%2 == 1:
-		print(i)
+import math 
+
+base = None 
+altura = None
+radio = None
+
+while True:
+    try:
+        base = float(input('Escribir base del triangulo:'))
+        break
+    except:
+        print('Escriba un número')
         
 
 
+while True:
+    try:
+        altura = float(input('Escribir altura del triangulo:'))
+        break
+    except:
+        print('Escriba un número')  
+        
+        
+area = base * altura /2
 
+print('El area del triángulo es:{}'.format(area))              
+
+while True:
+    try:
+        radio = float(input('Escribir radio del circulo:'))
+        break
+    except:
+        print('Escriba un número')
+
+
+        
+
+area = math.pi*(radio*radio)        
+
+
+print('El area del circulo es:{}'.format(area))            
 
 
 
